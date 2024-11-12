a1b710b30f4a5ebe4bfee1bdea4377a7e6f6fb7b
chrishyze <chrishyze@gmail.com>
2024-11-12 18:35:28 +0800

    Add unit tests

---
+++ tests/AtomFeed.Tests/SerializeTests.cs
@@ -0,0 +1,97 @@
+using System.Data;
+using AtomFeed.Element;
+
+namespace AtomFeed.Tests;
+
+public class SerializeTests
+{
+    [Fact]
+    public void EmptyFeedIdTest()
+    {
+        // Arrange
+        var feed = new Feed
+        {
+            Id = "",
+            Title = "",
+            Updated = DateTimeOffset.UtcNow
+        };
+
+        // Act
+        var caughtException = Assert.Throws<ConstraintException>(() => AtomFeed.Serialize(feed));
+
+        // Assert
+        Assert.Equal("AtomFeed: feed id can not be null or empty", caughtException.Message);
+    }
+
+    [Fact]
+    public void EmptyFeedTitleTest()
+    {
+        // Arrange
+        var feed = new Feed
+        {
+            Id = "id",
+            Title = "",
+            Updated = DateTimeOffset.UtcNow
+        };
+
+        // Act
+        var caughtException = Assert.Throws<ConstraintException>(() => AtomFeed.Serialize(feed));
+
+        // Assert
+        Assert.Equal("AtomFeed: feed title can not be null or empty", caughtException.Message);
+    }
+
+    [Fact]
+    public void EmptyEntryIdTest()
+    {
+        // Arrange
+        var feed = new Feed
+        {
+            Id = "id",
+            Title = "title",
+            Updated = DateTimeOffset.UtcNow,
+            Entries =
+            [
+                new Entry
+                {
+                    Id = "",
+                    Title = "",
+                    Updated = DateTimeOffset.UtcNow
+                }
+            ]
+        };
+
+        // Act
+        var caughtException = Assert.Throws<ConstraintException>(() => AtomFeed.Serialize(feed));
+
+        // Assert
+        Assert.Equal("AtomFeed: entry id can not be null or empty", caughtException.Message);
+    }
+
+    [Fact]
+    public void EmptyEntryTitleTest()
+    {
+        // Arrange
+        var feed = new Feed
+        {
+            Id = "id",
+            Title = "title",
+            Updated = DateTimeOffset.UtcNow,
+            Entries =
+            [
+                new Entry
+                {
+                    Id = "id",
+                    Title = "",
+                    Updated = DateTimeOffset.UtcNow
+                }
+            ]
+        };
+
+        // Act
+        var caughtException = Assert.Throws<ConstraintException>(() => AtomFeed.Serialize(feed));
+
+        // Assert
+        Assert.Equal("AtomFeed: entry title can not be null or empty", caughtException.Message);
+    }
+}
